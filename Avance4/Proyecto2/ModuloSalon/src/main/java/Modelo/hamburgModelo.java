fd7e69e03d33b44a2f97ee61acd68ba583e757fe
Alejandra Delgado <aledelgado1209@gmail.com>
2023-06-07 16:02:21 -0600

    Avance 4  listo simulacion, falta agregar cocina

---
+++ Avance4/Proyecto2/ModuloSalon/src/main/java/Modelo/hamburgModelo.java
@@ -0,0 +1,22 @@
+package Modelo;
+
+public class hamburgModelo {
+    public String descripcion;
+    public int precio;
+    
+    public String getDesc() {
+        return descripcion;
+    }
+    
+    public void setDesc(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    
+    public int getPrecio() {
+        return precio;
+    }
+    
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+}
