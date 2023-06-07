486b29d009b7ac7f07b8d52e8427e9616a44321e
Alejandra Delgado <aledelgado1209@gmail.com>
2023-06-07 16:43:24 -0600

    Avance 5- Agregue parte de cocina

---
+++ Avance 5/Proyecto2/ModuloSalon/src/main/java/Controlador/Ingredientes/ingreNuloC.java
@@ -0,0 +1,16 @@
+package Controlador.Ingredientes;
+
+import Controlador.Ingredientes.ingredientesC;
+
+public class ingreNuloC implements ingredientesC{
+    public ingreNuloC(){
+    }
+
+    public String descripcion(){
+        return "";
+    }
+
+    public int precio(){
+        return 0;
+    }
+}
